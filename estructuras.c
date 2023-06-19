461e597cff5e35b3dea88ca0a100c5a9c8dfb3d8
Roberto-Ulloa <111545962+RobertoUlloa123@users.noreply.github.com>
2023-06-19 15:33:57 -0500

    ponemos la estructura

--- estructuras.c
+++ estructuras.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
-int main() {
+int main() {struct alumno
+{
+    char nombre[20];
+    char direccion[20];
+    char carrera[20];
+    int edad;
+    float promedio;
+};
    
     
     return 0;
